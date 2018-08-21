2e5643fab5e24ff254a5ad9de01d298208c948b6
chendelian <116870791@qq.com>
2018-08-21 18:29:19 +0800

    新建算法

--- algorithm/src/main/java/com/work/Common.java
+++ algorithm/src/main/java/com/work/Common.java
@@ -1,4 +1,15 @@
 package com.work;
 
 public class Common {
+
+
+    public static void printSort(int[] numbers) {
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.print(numbers[i] + "\t");
+        }
+        System.out.println();
+
+    }
+
+
 }
--- algorithm/src/main/java/com/work/SelectSort.java
+++ algorithm/src/main/java/com/work/SelectSort.java
@@ -1,4 +1,21 @@
 package com.work;
 
+/**
+ * 选择排序算法
+ */
 public class SelectSort {
+
+    public static void main(String[] args) {
+        int[] numbers = {2, 3, 8, 1, 5, 10};
+        sort(numbers);
+        Common.printSort(numbers);
+    }
+
+
+    public static void sort(int[] numbers) {
+
+
+    }
+
+
 }
